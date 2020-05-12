d679698dd47f214303ece5dad1457e0b0e043ea3
Ndagi Stanley <ndagis@gmail.com>
2020-05-13 00:24:24 +0300

    Adding tests to the Task Manager application

---
+++ src/pages/Tab1.test.tsx
@@ -0,0 +1,15 @@
+// src/pages/Tab1.test.tsx
+
+import React from "react";
+import { render } from "@testing-library/react";
+import Tab1 from "./Tab1";
+
+test("Page title is Task Manager", async () => {
+    const { findByText } = render(<Tab1 />);
+    await findByText("Task Manager");
+});
+
+test("When there are no Tasks, inform the user that no tasks have been created", async () => {
+    const { findByText } = render(<Tab1 />);
+    await findByText("You are yet to add tasks for Today");
+});
